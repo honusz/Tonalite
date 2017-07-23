1183198b0dcde37dda49100c7fb6e69f43004f49
John Roper <johnroper100@gmail.com>
2017-07-22 21:57:01 -0400

    more ui work

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from sACN import DMXSource
 
 server_host = '192.168.0.106'
-server_port = 5000
+server_port = 6578
 files_location = "./"
 sacn_ip = "169.254.39.191"
 
 source = DMXSource(universe=1, net_ip=sacn_ip)
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'secret!'
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('layout.html')
 
 
 if __name__ == '__main__':
-    app.run(host=server_host, port=server_port)
+    app.run(host=server_host, port=server_port, debug=True)
