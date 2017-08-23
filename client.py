fe38d1680e1972b94f4dd6c4f8f710a9ee9fe2d3
John Roper <johnroper100@gmail.com>
2017-08-23 13:03:36 -0400

    more cvlient work

--- client.py
+++ client.py
@@ -14,6 +14,8 @@ channels = [0] * 48
 cues = []
 ccue = 0
 
+running = False
+
 
 def set_list(l, i, v):
     try:
@@ -24,14 +26,20 @@ def set_list(l, i, v):
         l[i] = v
 
 
-def generate_fade(start, end, secs=3.0, fps=40):
+def generate_fade(threadname, start, end, secs=3.0, fps=40):
+    global running
     for index in range(int(secs * fps)):
-        for channel in range(len(start)):
-            a = start[channel] or 0
-            b = end[channel] or 0
-            channels[channel] = int(a + (((b - a) / (secs * fps)) * index))
-        source.send_data(channels)
-        time.sleep(secs / (int(secs * fps)))
+        if running == True:
+            for channel in range(len(start[0])):
+                a = start[0][channel] or 0
+                b = end[0][channel] or 0
+                channels[channel] = int(a + (((b - a) / (secs * fps)) * index))
+            source.send_data(channels)
+            time.sleep(secs / (int(secs * fps)))
+        else:
+            running = False
+            break
+    running = False
 
 
 sio = socketio.AsyncServer(async_mode='aiohttp')
@@ -46,27 +54,60 @@ async def index(request):
 
 @sio.on('command message', namespace='/tonalite')
 async def test_message(sid, message):
+    global running
     global cues
     global channels
     global ccue
     cmd = message['data'].lower().split()
-    if len(cmd) == 3:
-        if cmd[0] == "q":
-            if cmd[2] == "r":
-                set_list(cues, int(cmd[1]) - 1, channels[:])
-            elif cmd[2] == "g":
-                generate_fade(cues[ccue], cues[int(cmd[1]) - 1])
-                ccue = int(cmd[1]) - 1
-    elif len(cmd) == 4:
-        if cmd[0] == "c":
-            if cmd[2] == "a":
-                value = int(cmd[3])
-                if value > 255:
-                    value = 255
-                elif value < 0:
-                    value = 0
-                set_list(channels, int(cmd[1]) - 1, value)
-                source.send_data(channels)
+    if running != False:
+        if len(cmd) == 2:
+            if cmd[0] == "q":
+                if cmd[1] == "s":
+                    running = False
+    else:
+        if len(cmd) == 2:
+            if cmd[0] == "q":
+                if cmd[1] == "n":
+                    running = True
+                    generate_fade(cues[ccue], cues[ccue + 1], secs=cues[ccue + 1][1])
+                    ccue = ccue + 1
+                elif cmd[1] == "l":
+                    running = True
+                    generate_fade(cues[ccue], cues[ccue - 1], secs=cues[ccue - 1][1])
+                    ccue = ccue - 1
+        elif len(cmd) == 3:
+            if cmd[0] == "q":
+                if cmd[2] == "r":
+                    set_list(cues, int(cmd[1]) - 1, [channels[:], 3.0])
+                elif cmd[2] == "g":
+                    running = True
+                    generate_fade(cues[ccue], cues[int(cmd[1]) - 1], secs=cues[int(cmd[1]) - 1][1])
+                    ccue = int(cmd[1]) - 1
+        elif len(cmd) == 4:
+            if cmd[0] == "c":
+                if cmd[2] == "a":
+                    value = cmd[3]
+                    if value != "d" and value != "b":
+                        value = int(cmd[3])
+                        if value > 255:
+                            value = 255
+                        elif value < 0:
+                            value = 0
+                    elif value == "d":
+                        value = channels[int(cmd[1]) - 1] - 10
+                        if value < 0:
+                            value = 0
+                    elif value == "b":
+                        value = channels[int(cmd[1]) - 1] + 10
+                        if value > 255:
+                            value = 255
+                    else:
+                        value = 0
+                    set_list(channels, int(cmd[1]) - 1, value)
+                    source.send_data(channels)
+            elif cmd[0] == "q":
+                if cmd[2] == "t":
+                    cues[int(cmd[1]) - 1][1] = float(cmd[3])
 
     await sio.emit('my response', {'data': 'Command recieved and processed!'}, room=sid,
                    namespace='/tonalite')
