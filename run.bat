b85c426ee40c04407c48b8309f986db2d01bf40d
John Roper <johnroper100@gmail.com>
2017-09-10 18:10:52 -0400

    Add the start of the app

---
+++ run.bat
@@ -0,0 +1,1 @@
+python36 tonalite.py
--- tonalite.py
+++ tonalite.py
@@ -1,9 +1,30 @@
 from sACN import DMXSource
 from tkinter import *
 
+
+class App:
+    def __init__(self, master):
+        Label(master, text="Tonalite IP:").grid(row=0)
+        Label(master, text="sACN IP:").grid(row=1)
+
+        self.app_ip = Entry(master)
+        self.sacn_ip = Entry(master)
+
+        self.app_ip.grid(row=0, column=1)
+        self.sacn_ip.grid(row=1, column=1)
+
+        self.quitBtn = Button(master, text="Quit", fg="red",
+                              command=master.quit).grid(row=2, column=0)
+
+        self.startBtn = Button(master, text="Start",
+                               command=self.start).grid(row=2, column=1)
+
+    def start(self):
+        print("hi there, everyone!")
+
+
 root = Tk()
 
-w = Label(root, text="Hello, world!")
-w.pack()
+app = App(root)
 
 root.mainloop()
