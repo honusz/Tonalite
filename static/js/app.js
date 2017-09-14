f2fd7081845ca11639fab315d1ddead1a6ece65e
John Roper <johnroper100@gmail.com>
2017-09-13 20:38:00 -0400

    More work

--- static/js/app.js
+++ static/js/app.js
@@ -1,39 +1,6 @@
-Vue.component('Modal', {
-    template: '#modal-template',
-    props: ['show'],
-    methods: {
-        close: function () {
-            this.$emit('close');
-        }
-    }
-});
-
-Vue.component('NewPostModal', {
-    template: '#new-post-modal-template',
-    props: ['show'],
-    data: function () {
-        return {
-            title: '',
-            body: ''
-        };
-    },
-    methods: {
-        close: function () {
-            this.$emit('close');
-            this.title = '';
-            this.body = '';
-        },
-        savePost: function () {
-            // Some save logic goes here...
-
-            this.close();
-        }
-    }
-});
-
 new Vue({
     el: '#app',
     data: {
-        showNewPostModal: false
+
     }
 });
