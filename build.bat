bfc0ec744da055c29edff36d7b815f4c1e7bb917
John Roper <johnroper100@gmail.com>
2018-02-01 10:28:40 -0500

    Update documentation build

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-cd docs && gitbook pdf . Tonalite-Documentation.pdf && cd ../ && cd src_channelman && build.bat && cd ../ && python3 -m PyInstaller tonalite.spec
+cd docs && gitbook pdf . Tonalite-v1.0.0-Documentation.pdf && cd ../ && cd src_channelman && build.bat && cd ../ && python3 -m PyInstaller tonalite.spec
