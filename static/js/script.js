3b64207aa232b45aa66a1d9391b2d73d165a770c
John Roper <johnroper100@gmail.com>
2018-08-26 08:13:42 -0400

    Fix tab display on the server size

--- static/js/script.js
+++ static/js/script.js
@@ -65,13 +65,13 @@ function openTab(evt, tabName) {
 
     // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+    //for (i = 0; i < tablinks.length; i++) {
+    //    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    //}
 
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+    //evt.currentTarget.className += " active";
 }
 
 function titleCase(str) {
