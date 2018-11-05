2bb2e16e111c3e940f92fdfe40840a02bb8810e3
John Roper <johnroper100@gmail.com>
2018-11-05 09:04:23 -0500

    add active color highlight to tab links

--- static/js/script.js
+++ static/js/script.js
@@ -342,15 +342,20 @@ function openTab(tabName) {
         tabcontent[i].style.display = "none";
     }
 
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    //for (i = 0; i < tablinks.length; i++) {
-    //    tablinks[i].className = tablinks[i].className.replace(" active", "");
-    //}
+    if (document.getElementsByClassName("tablinks-" + tabName)[0]) {
+        // Get all elements with class="tablinks" and remove the class "active"
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].classList.remove("active");
+        }
+    }
 
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(tabName).style.display = "block";
-    //evt.currentTarget.className += " active";
+
+    if (document.getElementsByClassName("tablinks-" + tabName)[0]) {
+        document.getElementsByClassName("tablinks-" + tabName)[0].classList.add("active");
+    }
 }
 
 function titleCase(str) {
