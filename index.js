b023b172d5c8a55d7c68c4917ecc2348ab8590a2
John Roper <johnroper100@gmail.com>
2018-11-13 17:15:33 -0500

    reset groups when reset fixtures

--- index.js
+++ index.js
@@ -659,6 +659,7 @@ io.on('connection', function (socket) {
     socket.on('resetFixtures', function () {
         if (fixtures.length != 0) {
             resetFixtures();
+            resetGroups();
             io.emit('fixtures', cleanFixtures());
             socket.emit('message', { type: "info", content: "Fixture values have been reset!" });
             saveShow();
