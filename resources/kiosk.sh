1c6840e8ad0f5b1bf3eb2da4a314cbaa0ef2e907
John Roper <johnroper100@gmail.com>
2018-12-29 07:57:50 -0500

    update kiosk script

--- resources/kiosk.sh
+++ resources/kiosk.sh
@@ -2,5 +2,7 @@
 # If Chrome crashes (usually due to rebooting), clear the crash flag so we don't have the annoying warning bar
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+sudo ip link set eth0 up
+sudo ip addr add 10.66.66.1/24 dev eth0
 /usr/local/bin/npm start --prefix /home/pi/Tonalite &
-chromium-browser --disable-pinch --overscroll-history-navagation=0 --kiosk http://localhost:3000
+chromium-browser --disable-pinch --overscroll-history-navagation=0 --kiosk http://10.66.66.1:3000
