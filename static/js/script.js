a1bba6c738b3d5e2eac806ffd24a85057b950543
John Roper <johnroper100@gmail.com>
2019-01-13 19:31:50 -0500

    add search to fixture profiles

--- static/js/script.js
+++ static/js/script.js
@@ -578,6 +578,25 @@ function upperCase(str) {
     return str.toUpperCase().replace(/-/g, " ");
 }
 
+function searchFixtureProfiles() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('searchFixtureProfiles');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("fixtureProfilesList");
+    li = ul.getElementsByTagName('li');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
 $('.custom-file-input').change(function () {
     var fileName = $(this).val().split('\\').pop();
     $(this).next('.custom-file-label').html(fileName);
