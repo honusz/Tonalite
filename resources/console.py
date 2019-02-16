4a7d036e1fa533fefff3b53d2f9ca7a7e3d24644
John Roper <johnroper100@gmail.com>
2019-02-16 15:10:40 -0500

    add handlers for the channel encoders

--- resources/console.py
+++ resources/console.py
@@ -6,11 +6,36 @@ from gpiozero import Button
 
 sio = socketio.Client()
 
-nextBtn = Button(17)
-stopRecordBtn = Button(27)
-lastBtn = Button(22)
-nextFixtureBtn = Button(23)
-lastFixtureBtn = Button(24)
+nextBtn = Button(16)
+lastBtn = Button(21)
+stopRecordBtn = Button(20)
+
+nextFixtureBtn = Button(19)
+lastFixtureBtn = Button(26)
+
+chan1EncUp = Button(13)
+chan1EncDown = Button(12)
+chan1Btn = Button(6)
+
+chan2EncUp = Button(5)
+chan2EncDown = Button(7)
+chan2Btn = Button(8)
+
+chan3EncUp = Button(11)
+chan3EncDown = Button(9)
+chan3Btn = Button(25)
+
+chan4EncUp = Button(10)
+chan4EncDown = Button(24)
+chan4Btn = Button(23)
+
+chan5EncUp = Button(22)
+chan5EncDown = Button(27)
+chan5Btn = Button(18)
+
+chan6EncUp = Button(17)
+chan6EncDown = Button(15)
+chan6Btn = Button(14)
 
 fixtures = []
 currentFixture = 0
@@ -18,7 +43,6 @@ currentFixtureChans = {}
 
 
 def sendNextCue():
-    print("next")
     sio.emit('nextCue')
 
 
@@ -51,8 +75,51 @@ def sendGetLastFixtureChans():
 lastBtn.when_pressed = sendNextCue
 stopRecordBtn.when_pressed = sendStopCue
 lastBtn.when_pressed = sendLastCue
+
 nextFixtureBtn.when_pressed = sendGetNextFixtureChans
 lastFixtureBtn.when_pressed = sendGetLastFixtureChans
+
+def chan1EncUpRising():
+    if chan1EncUp.is_pressed: print(-1)
+def chan1EncDownRising():
+    if chan1EncDown.is_pressed: print(1)
+chan1EncUp.when_pressed = chan1EncUpRising
+chan1EncDown.when_pressed = chan1EncDownRising
+
+def chan2EncUpRising():
+    if chan2EncUp.is_pressed: print(-1)
+def chan2EncDownRising():
+    if chan2EncDown.is_pressed: print(1)
+chan2EncUp.when_pressed = chan2EncUpRising
+chan2EncDown.when_pressed = chan2EncDownRising
+
+def chan3EncUpRising():
+    if chan3EncUp.is_pressed: print(-1)
+def chan3EncDownRising():
+    if chan3EncDown.is_pressed: print(1)
+chan3EncUp.when_pressed = chan3EncUpRising
+chan3EncDown.when_pressed = chan3EncDownRising
+
+def chan4EncUpRising():
+    if chan4EncUp.is_pressed: print(-1)
+def chan4EncDownRising():
+    if chan4EncDown.is_pressed: print(1)
+chan4EncUp.when_pressed = chan4EncUpRising
+chan4EncDown.when_pressed = chan4EncDownRising
+
+def chan5EncUpRising():
+    if chan5EncUp.is_pressed: print(-1)
+def chan5EncDownRising():
+    if chan5EncDown.is_pressed: print(1)
+chan5EncUp.when_pressed = chan5EncUpRising
+chan5EncDown.when_pressed = chan5EncDownRising
+
+def chan6EncUpRising():
+    if chan6EncUp.is_pressed: print(-1)
+def chan6EncDownRising():
+    if chan6EncDown.is_pressed: print(1)
+chan6EncUp.when_pressed = chan6EncUpRising
+chan6EncDown.when_pressed = chan6EncDownRising
 
 @sio.on('connect')
 def on_connect():
