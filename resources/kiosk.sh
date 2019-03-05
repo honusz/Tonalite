4fa47a44437c8e0c85ad4029d6dd106a670890b4
John Roper <johnroper100@gmail.com>
2019-03-05 06:24:01 -0500

    update kiosk and splash

--- resources/kiosk.sh
+++ resources/kiosk.sh
@@ -3,6 +3,6 @@
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 sudo ip link set eth0 up
-sudo ip addr add 10.66.66.1/24 dev eth0
+sudo ip addr add 192.168.0.123/24 dev eth0
 /usr/local/bin/npm start --prefix /home/pi/Tonalite &
-chromium-browser --disable-pinch --overscroll-history-navagation=0 --kiosk http://10.66.66.1:3000
+chromium-browser --disable-pinch --overscroll-history-navagation=0 --kiosk http://192.168.0.123:3000
