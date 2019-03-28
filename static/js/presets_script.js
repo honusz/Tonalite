487e73ae6f97bad9638171899ed712b43744f0fa
John Roper <johnroper100@gmail.com>
2019-03-28 09:52:55 -0400

    add grandmaster and disconnected modal to presets view

--- static/js/presets_script.js
+++ static/js/presets_script.js
@@ -3,13 +3,34 @@ var socket = io('http://' + document.domain + ':' + location.port);
 var app = new Vue({
     el: '#app',
     data: {
-        presets: []
+        presets: [],
+        grandmaster: 0.0,
+        desktop: false
     },
     methods: {
         changePresetActive: function (presetID) {
             socket.emit('changePresetActive', presetID);
-        }
+        },
+        updateGrandmasterValue: function () {
+            socket.emit('changeGrandmasterValue', app.grandmaster);
+        },
     }
+});
+
+socket.on('connect', function () {
+    $('#serverDisconnectedModal').modal("hide");
+});
+
+socket.on('connect_error', function () {
+    $('#serverDisconnectedModal').modal("show");
+});
+
+socket.on('meta', function (metadata) {
+    app.desktop = metadata.desktop;
+});
+
+socket.on('grandmaster', function (value) {
+    app.grandmaster = value;
 });
 
 socket.on('presets', function (presets) {
